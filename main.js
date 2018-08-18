921e07248a9ee3e916f0e2cd503280da087ac377
stharuma <stharuma@gmail.com>
2018-08-18 12:40:48 -0400

    variable type

--- main.js
+++ main.js
@@ -9,3 +9,42 @@ console.log(x + '  ' + y);
 //const y=77;// Cannot redeclare block-scoped variable 'y'.
 //const str //'str' is declared but its value is never read.
 //  'const' declarations must be initialized.
+// Variable Type
+var isStart = true;
+var num = 100;
+var name = 'stharuma';
+var note = "I am " + name + " \n         and developing web app";
+console.log(note);
+//null and undefined
+var n = null;
+var u = undefined;
+//array declaration
+var list1 = [1, 2, 3, 4, 5];
+var list2 = [1, 2, 3, 4, 5];
+var person1 = ['Max', 34];
+//enum color {Red, Green, Pink}
+var color;
+(function (color) {
+    color[color["Red"] = 4] = "Red";
+    color[color["Green"] = 5] = "Green";
+    color[color["Pink"] = 6] = "Pink";
+})(color || (color = {}));
+var c = color.Pink;
+console.log('enum ' + c);
+var randomvalue = 19;
+randomvalue = true;
+randomvalue = 'stharuma';
+//any variable type work with following print messages
+console.log(randomvalue.name);
+console.log(randomvalue());
+console.log(randomvalue.toUppercase());
+// unknown type
+var myVariable = 46;
+console.log(myVariable.toUpperCase());
+;
+function hasName(obj) {
+    return !!obj && typeof obj === 'object' && name in obj;
+}
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
--- main.ts
+++ main.ts
@@ -9,3 +9,43 @@ console.log(x +'  '+y);
 //const y=77;// Cannot redeclare block-scoped variable 'y'.
 //const str //'str' is declared but its value is never read.
          //  'const' declarations must be initialized.
+
+         // Variable Type
+         let isStart:boolean =true;
+         let num: number =100;
+         let name: string ='stharuma';
+         let note: string =`I am ${name} 
+         and developing web app`;
+         console.log(note);
+         //null and undefined
+         let n:null =null;
+         let u:undefined=undefined;
+         //array declaration
+         let list1:number[]=[1,2,3,4,5];
+         let list2:Array<number>=[1,2,3,4,5];
+
+         let person1:[string,number]=['Max', 34];
+
+         //enum color {Red, Green, Pink}
+         enum color {Red=4, Green, Pink}
+         let c:color =color.Pink;
+         console.log('enum '+c);
+
+         let randomvalue :any =19;
+         randomvalue =true;
+         randomvalue ='stharuma';
+         //any variable type work with following print messages
+         console.log(randomvalue.name);
+         console.log(randomvalue());
+         console.log(randomvalue.toUppercase());
+         // unknown type
+         let myVariable:unknown = 46;
+         console.log((myVariable as string).toUpperCase()));
+
+function hasName(obj:any): obj is {name}{
+   return !!obj && typeof obj === 'object' && name in obj;
+}
+if(hasName(myVariable)){
+    console.log(myVariable.name);
+}
+
