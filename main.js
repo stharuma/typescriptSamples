05e3248f2627a4fcd9fedaa09269a949c66ac597
stharuma <stharuma@gmail.com>
2018-08-18 11:04:45 -0400

    Variable declaration

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 exports.__esModule = true;
 var message = 'Welcome to Typescript';
 console.log(message);
+var x = 30;
+var y = 45;
+console.log(x + '  ' + y);
+//let x = 33;//Cannot redeclare block-scoped variable 'x'.
+//const y=77;// Cannot redeclare block-scoped variable 'y'.
+//const str //'str' is declared but its value is never read.
+//  'const' declarations must be initialized.
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 export {}
 let message= 'Welcome to Typescript';
 console.log(message);
+
+let x =30;
+const y =45;
+console.log(x +'  '+y);
+//let x = 33;//Cannot redeclare block-scoped variable 'x'.
+//const y=77;// Cannot redeclare block-scoped variable 'y'.
+//const str //'str' is declared but its value is never read.
+         //  'const' declarations must be initialized.
